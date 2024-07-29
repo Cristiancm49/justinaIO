afde589f378bc99c564f6c100d473bda9bae1ef7
ccortesm49 <ccortesm2000@gmail.com>
2024-07-29 03:37:07 -0500

    Se suben todo el codigo para el deploy

---
+++ src/Pages/App/App.jsx
@@ -0,0 +1,40 @@
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import React from "react";
+import DoctorRegistry from "../DoctorRegistry";
+import LogIn from "../LogIn";
+import "./App.css";
+import PatientProfile from "../PatientProfile/Index";
+
+import EmailResetPassword from '../resetPassword';
+import LandingPage from "../landing";
+import  Home  from "../home";
+
+
+
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/registro', element: <DoctorRegistry /> },
+    { path: '/login', element: <LogIn /> },
+    { path: '/patientProfile', element: <PatientProfile/>},
+    {path: '/resetPassword', element: <EmailResetPassword />},
+    {path: '/', element: <LandingPage />},
+    { path: '/resetPassword', element: <EmailResetPassword />},
+    {path:'/home', element: <Home />},
+    
+  ])
+
+  return routes
+};
+
+function App() {
+  return (
+    <div className="bg-custom-bg bg-cover bg-center">
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
