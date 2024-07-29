e3025e8499f3c2f165314540e5c5a5438298f941
ccortesm49 <ccortesm2000@gmail.com>
2024-07-29 12:41:36 -0500

    k

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true, // Desactiva sourcemaps
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
